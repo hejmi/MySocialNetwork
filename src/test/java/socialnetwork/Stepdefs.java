e87e0b89409ceabacd001b5fa3bd8d1b51471f23
MichMatch <michael@graphicdesire.se>
2020-12-10 12:07:40 +0100

    refactored Scenario 2 with correct cucumber data and H2Dao

--- src/test/java/socialnetwork/Stepdefs.java
+++ src/test/java/socialnetwork/Stepdefs.java
@@ -11,11 +11,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Stepdefs {
     public List <TimeLine> theTimeLine = new ArrayList<>();
-    private TimeLineH2Dao h2Conn;
+    private TimeLineH2Dao h2Conn = new TimeLineH2Dao();
 
     @Given("user {string} exists and posts one message")
     public void userExistsAndPostsMessage(String arg0) {
-        h2Conn = new TimeLineH2Dao();
         h2Conn.checkIfUserExists(arg0);
     }
 
@@ -33,21 +32,24 @@ public class Stepdefs {
         assertEquals(List.of("Hello world"), h2Conn.showTimeLine(arg0));
     }
 
-    @Given("posting two messages to personal timeline")
-    public void postingTwoMessagesToPersonalTimeline() {
-        //timeLine = new TimeLine();
+    @Given("user {string} exists and posts two messages")
+    public void postingTwoMessagesToPersonalTimeline(String arg0) {
+        h2Conn.checkIfUserExists(arg0);
     }
 
     @When("{string} posts {string} and {string}")
     public void postsAnd(String arg0, String arg1, String arg2) {
-        theTimeLine.add(new TimeLine(arg0, arg1));
-        theTimeLine.add(new TimeLine(arg0, arg2));
+        //theTimeLine.add(new TimeLine(arg0, arg1));
+        //theTimeLine.add(new TimeLine(arg0, arg2));
+        h2Conn.publish(new TimeLine(arg0, arg1));
+        h2Conn.publish(new TimeLine(arg0, arg2));
     }
 
-    @Then("the posts should be published on personal timeline")
-    public void thePostsShouldBePublishedOnPersonalTimeline() {
-        assertEquals("Hello world", theTimeLine.get(0).postedmessage);
-        assertEquals("I'm happy today", theTimeLine.get(1).postedmessage);
+    @Then("the posts should be published on {string}'s timeline")
+    public void thePostsShouldBePublishedOnPersonalTimeline(String arg0) {
+        //assertEquals("Hello world", theTimeLine.get(0).postedmessage);
+        //assertEquals("I'm happy today", theTimeLine.get(1).postedmessage);
+        assertEquals(List.of("Hello world", "I'm happy today"), h2Conn.showTimeLine(arg0));
     }
 
     @Given("Bob wants to read Alice's timeline")
--- src/test/resources/gurka/socialnetwork.feature
+++ src/test/resources/gurka/socialnetwork.feature
@@ -6,24 +6,23 @@ Feature: Social Networking is fun
     Then the post should be published on "Alice"'s timeline
 
   Scenario: A user can post two messages to personal timeline
-    Given posting two messages to personal timeline
-    Given user "Alice" exists and posts 2 messages
+    Given user "Alice" exists and posts two messages
     When "Alice" posts "Hello world" and "I'm happy today"
-    Then the posts should be published on personal timeline
+    Then the posts should be published on "Alice"'s timeline
 
   Scenario: A user wants to read another users timeline
     Given Bob wants to read Alice's timeline
-    Given user "Bob" exists and reads "Alice"s timeline
+    Given user "Bob" and "Alice" exists
     When "Bob" enters "Alice"'s timeline
     Then he should see all the published messages from "Alice"
 
    Scenario: Following another user
      Given A user wants to follow another user
-     Given user "Charlie" is following "Alice" and "Bob"
+     Given user "Charlie", "Alice" and "Bob" exists
      When "Charlie" subscribes to the timelines for "Alice" and "Bob"
      Then it will return an aggregated list of subscriptions for "Charlie"
 
    Scenario: Mentions of another user on personal timeline
-     Given user "Bob" links to "Charlie" in a post
+     Given user "Bob" and "Charlie" exists
      When "Bob" posts a message containing @"Charlie"
      Then "Bob"s post should include a link to "Charlie"
